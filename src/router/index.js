0c87330adb71f0fb556d07e6a435fc2e57b1e8e4
“zvhao” <nguyenvanhao29102001@gmail.com>
2023-02-24 18:49:26 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
 		component: () => import("@/views/ContactEdit.vue"),
 		props: true // Truyền các biến trong $route.params vào làm props
 	},
+	{
+		path: "/contacts/add",
+		name: "contact.add",
+		component: () => import("@/views/ContactAdd.vue"),
+	},
 ];
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
